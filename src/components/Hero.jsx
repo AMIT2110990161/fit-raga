7f0f4f79669997b5603597943bdf1033a731844d
AMIT KUMAR <amit70kumar14@gmail.com>
2024-06-22 02:05:29 +0530

    prop

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,14 +5,14 @@ import { Link } from 'react-router-dom'
 
 export default function Hero() {
     return (
-        <div className='min-h-screen flex flex-col gap-10 items-center justify-center text-center max-w-[800px] w-full mx-auto p-4 ' style={{ marginTop: '-40px' }}>
+        <div className='min-h-screen flex flex-col gap-10 items-center justify-center text-center max-w-[800px] w-full mx-auto p-4'>
             <div className='flex flex-col gap-4'>
 
                 <p>IT'S TIME TO GET</p>
                 <h1 className='font-semibold text-4xl sm:text-5xl md:text-6xl lg:text-7xl'>RAGA<span className='text-blue-400'> &fitness</span></h1>
             </div>
             <p className='text-sm md:text-base font-light'>Stop making excuses and start making gains with <span className='text-blue-400 font-medium'>RAGA &fitness. <br /> </span> Push harder, lift heavier, and become unstoppable. Your best body is waiting – claim it now!</p>
-            <div className='flex gap-10'>
+            <div className='flex gap-5'>
                 <Link to={'/Generator'}>
                     <Button text={"Plan your Workouts"}></Button>
                 </Link>
